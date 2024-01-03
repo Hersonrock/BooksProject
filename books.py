32e9b3d7de863563ddf2b19da4c88e5cccbd8993
Hersonrock <hjpg20@gmail.com>
2024-01-03 11:43:38 -0500

    Adds a basic books dict and a read all GET

--- books.py
+++ books.py
@@ -2,6 +2,29 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+books ={
+    "book1":{
+        "title":"Title One",
+        "author":"Author One",
+             },
+    "book2":{
+        "title":"Title Two",
+        "author":"Author Two",
+             },
+    "book3":{
+        "title":"Title Three",
+        "author":"Author Three",
+             },
+    "book4":{
+        "title":"Title Four",
+        "author":"Author Four",
+             },
+    "book5":{
+        "title":"Title Five",
+        "author":"Author Five",
+             },
+}
+
 @app.get("/")
-async def hello():
-    return {"message" : "Hello, World"}
+async def read_all_books():
+    return books
